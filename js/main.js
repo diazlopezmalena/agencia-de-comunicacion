dae421eabb7c019b4d58fec40f3e9e42af6a6d22
diazlopezmalena <85799626+diazlopezmalena@users.noreply.github.com>
2022-05-24 20:50:57 -0300

    menu plegable segun scroll con js

--- js/main.js
+++ js/main.js
@@ -41,5 +41,31 @@ $(document).ready(function(){
     $('.navbar-nav>li>a').on('click', function(){
         $('.navbar-toggler').addClass('collapsed');
         $('.navbar-collapse').removeClass('show').addClass('hide');
-    })
+        $('body').removeClass('scroll-up');
+    });
+    
+    // Ocultar y mostrar nav según scroll
+    const body = document.body;
+    const scrollUp = "scroll-up";
+    const scrollDown = "scroll-down";
+    let lastScroll = 0;
+    
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
+        if (currentScroll == 0) {
+            body.classList.remove(scrollUp);
+            return;
+        }
+        
+        if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+            // down
+            body.classList.remove(scrollUp);
+            body.classList.add(scrollDown);
+        } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+            // up
+            body.classList.remove(scrollDown);
+            body.classList.add(scrollUp);
+        }
+        lastScroll = currentScroll;
+    });
 });
